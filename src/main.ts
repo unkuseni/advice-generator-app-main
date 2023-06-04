4d128fe56735756894a3087b5701636c6a918f96
unkuseni <amudalatadeoye@gmail.com>
2023-06-04 01:00:17 +0100

    got the api working

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,10 @@
-let card: HTMLElement | null = document.getElementById("app");
-// let the text content of the card be "Hello World"
-let svgDiv = document.createElement("div");
-svgDiv.setAttribute("class", "divider");
-let header = document.createElement("header");
-let quote = document.createElement("blockquote");
-let advice: string = "\'Just do it!'";
-header.textContent = "Advice #001";
-quote.textContent = `${advice}`;
-if (card) {
-  card.appendChild(header);
-  card.appendChild(quote);
-  card.appendChild(svgDiv);
+const generateAdvice = () => {
+  fetch('https://ron-swanson-quotes.herokuapp.com/v2/quotes')
+  .then(response => response.json())
+  .then(data => console.log(data[0]))
+  .catch(error => console.error(error));
 };
-console.log(card);
+let button = document.getElementById("generate");
+if (button) {
+  button.onclick = generateAdvice;
+};
